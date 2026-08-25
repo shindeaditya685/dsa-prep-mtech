6df4ae8776c7c655d499d8a0182d3264ebd8ef71
Aditya Shinde <82741617+shindeaditya685@users.noreply.github.com>
2026-08-25 12:18:37 +0530

    Add QuickSort implementation in QuickSort.java
    Implement QuickSort algorithm for sorting an array.

---
+++ QuickSort.java
@@ -0,0 +1,53 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+    
+    
+    static void quickSort(int[] arr, int low, int high) {
+        if (low < high) {
+            int pivotIndex = partition(arr, low, high);
+            quickSort(arr, low, pivotIndex - 1);
+            quickSort(arr, pivotIndex + 1, high);
+        }
+    }
+    
+    static int partition(int[] arr, int low, int high) {
+        int pivot = arr[high];
+        int i = low - 1;
+        
+        for (int j = low; j < high; j++) {
+            if (arr[j] <= pivot) {
+                i++;
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+        
+        int temp = arr[i + 1];
+        arr[i + 1] = arr[high];
+        arr[high] = temp;
+        
+        return i + 1;
+        
+    }
+    
+    
+    
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		int[] arr = {3, 8, 1, 45, 12, 5};
+		
+		
+		quickSort(arr, 0, arr.length - 1);
+		
+		for (int num : arr) {
+		    System.out.print(num + " ");
+		}
+
+	}
+}
